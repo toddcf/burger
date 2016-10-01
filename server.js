b7ef8a67a4ef464d55dc9a8964033e41c7267829
Todd Croak-Falen <Todd Croak-Falen>
2016-09-30 18:38:57 -0700

    Added bodyParsers to server.js.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express 		= require('express');
 var methodOverride 	= require('method-override');
 var bodyParser 		= require('body-parser');
+var path			= require('path');
 
 // Express Setup
 var app 	= express();
@@ -9,6 +10,12 @@ var PORT 	= process.env.PORT || 3000;
 
 // Allows Express To Use Static Files
 app.use(express.static(path.join(__dirname, 'app/public')));
+
+// Allows Express To Parse Data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 
 // Starts The App
 app.listen(PORT, function () {
