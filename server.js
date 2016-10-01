3a6f9592e9a6e0096843136fd0f8f4e4c6479c4c
Todd Croak-Falen <Todd Croak-Falen>
2016-09-30 18:25:19 -0700

    Set up server.js file.

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-var express = require('express');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
+// Node Modules
+var express 		= require('express');
+var methodOverride 	= require('method-override');
+var bodyParser 		= require('body-parser');
+
+// Express Setup
+var app 	= express();
+var PORT 	= process.env.PORT || 3000;
+
+// Allows Express To Use Static Files
+app.use(express.static(path.join(__dirname, 'app/public')));
+
+// Starts The App
+app.listen(PORT, function () {
+	console.log("App listening on Port " + PORT);
+});
